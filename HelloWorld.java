899c909de835903971dd5ea5893d21fec5d77aa6
ChristianNwabuko <crissbuko@yahoo.com>
2016-04-03 22:30:48 -0500

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * Program description - Prints "Hello World" on screen
+ * Christian Nwabuko
+ * 2/03/16
+ * JDK version 1.8.0_71
+ * ****************************************************************************/
+
+public class HelloWorld 
+{
+    public static void main(String[] args){
+        System.out.println("Helllo World");
+        System.out.println("Christian Nwabuko");
+    }
+}
